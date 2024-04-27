d14aa32ca068753d4da06a106cebe718f738a7c5
Nicolas Arancibia <nj.arancibiah@gmail.com>
2024-04-26 23:45:05 -0400

    subiendo a github

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
